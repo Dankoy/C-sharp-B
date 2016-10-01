d281d328f6f90f3862528ac7fc85ecf02faf01c4
Evgeny Zelenin <transingularity@gmail.com>
2016-10-01 20:32:44 +0400

    Added project with three constructors with datetime parameters

---
+++ Lab1/SelfMade/DateObj.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfMade
+{
+    class DateObj
+    {
+        DateTime dateTime;
+        public DateObj()
+        {
+            dateTime = new System.DateTime(DateTime.Now.Ticks);
+            Console.WriteLine(dateTime);
+        }
+
+        public DateObj(string date)
+        {
+            dateTime = Convert.ToDateTime(date);
+            Console.WriteLine(dateTime.ToShortDateString());
+        }
+        public DateObj(string date, string time)
+        {
+            Console.WriteLine(date + " " + time);
+        }
+    }
+}
---
+++ Lab1/SelfMade/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfMade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateObj obj1 = new DateObj();
+            DateObj obj2 = new DateObj(System.DateTime.Now.ToShortDateString());
+            DateObj obj3 = new DateObj(System.DateTime.Now.ToShortDateString(), System.DateTime.Now.ToShortTimeString());
+        }
+    }
+}
