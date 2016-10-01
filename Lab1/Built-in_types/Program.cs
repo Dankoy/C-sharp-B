bd0f10c20c7feefe5cb8c206e686fa42f60c094d
Evgeny Zelenin <transingularity@gmail.com>
2016-10-01 18:58:24 +0400

    Added project using built-in types

---
+++ Lab1/Built-in_types/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Built_in_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 55;
+            Console.WriteLine(myInt.GetType().ToString());
+
+            float myFloat = 123;
+            Console.WriteLine(myFloat.GetType().ToString());
+
+            float myFloat2 = (float)123.5;
+            Console.WriteLine("{0}   {1}   {2}", myInt, myFloat, myFloat2);
+        }
+    }
+}
