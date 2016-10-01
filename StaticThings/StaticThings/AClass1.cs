70629320a17dfe651760a5fe66a3a5487c5ff591
Evgeny Zelenin <transingularity@gmail.com>
2016-10-01 16:19:20 +0400

    Added different date format output

--- StaticThings/StaticThings/AClass1.cs
+++ StaticThings/StaticThings/AClass1.cs
@@ -10,7 +10,11 @@ namespace StaticThings
         public static string M1()
         {
             System.DateTime dateTime = new System.DateTime(DateTime.Now.Ticks);
-            System.Console.WriteLine("{0:d}", dateTime);
+            System.Console.WriteLine("Show dateTime using '(0:D)' {0:D}", dateTime);
+            System.Console.WriteLine("Show dateTime using '(0:d)' {0:d}", dateTime);
+            System.Console.WriteLine("Show dateTime using '(0:f)' {0:f}", dateTime);
+            System.Console.WriteLine("Show dateTime using '(0:F)' {0:F}", dateTime);
+            System.Console.WriteLine("Show dateTime using '(0:G)' {0:G}", dateTime);
             return dateTime.ToString();
         }
     }
