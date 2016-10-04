358cc127102c5e843a9d09925a713f6e57d15d96
Dankoy <transingularity@gmail.com>
2016-10-04 21:18:32 +0300

    Added project with simple usage of LINQ. Added two arrays as data sources.

---
+++ Lab2/LINQ_Using/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Using
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Will be used as data sources
+            int[] numbers = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
+            string[] words = { "apple", "orange", "cherry" };
+        }
+    }
+}
