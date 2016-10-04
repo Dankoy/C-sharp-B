a680dff23795f34823f4d9d9ef392fc6303e5da6
Dankoy <transingularity@gmail.com>
2016-10-04 20:53:47 +0300

    Added two arrays and write in console their dimensions

--- Lab2/Arrays/Program.cs
+++ Lab2/Arrays/Program.cs
@@ -15,6 +15,14 @@ namespace Arrays
             a1.iArray[0] = 100;
             Console.WriteLine(a1.iArray[0]);
             Console.WriteLine(a1.iArray.GetLength(0));
+
+            int[,] dArr = new int[6, 3];
+            int[,] dArr2 = new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+            Console.WriteLine("Elements in array dArr = {0}\n First dimension {1}\n Second dimension {2}", dArr.Length, dArr.GetLength(0), dArr.GetLength(1));
+            Console.WriteLine("Elements in array dArr2 = {0}\n First dimension {1}\n Second dimension {2}", dArr2.Length, dArr2.GetLength(0), dArr2.GetLength(1));
+            Console.WriteLine("Amount of dimensions of array dArr2 = {0}", dArr2.Rank);
+            
+           
         }
     }
 }
