17540cf85462fb8b367b5de84f21aeb8694990ec
Dankoy <transingularity@gmail.com>
2016-10-18 21:08:57 +0300

    Added class MyAttribute with two fields

---
+++ Task2/Indexation/MyAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// My custom attribute
+
+namespace Indexation
+{
+    class MyAttribute : System.Attribute
+    {
+        private string name;
+        private int kod;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+        public int Kod
+        {
+            get
+            {
+                return kod;
+            }
+            set
+            {
+                kod = value;
+            }
+        }
+    }
+}
