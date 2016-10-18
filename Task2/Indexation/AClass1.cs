9c925a4119601959856356bf96f26c7e06ab9b96
Dankoy <transingularity@gmail.com>
2016-10-18 20:51:25 +0300

    Added new Task with indexator

---
+++ Task2/Indexation/AClass1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexation
+{
+    class AClass1
+    {
+        private int[] imyArray = new int[20]; //Collection
+
+        public int this[int i]  //Indexator for collection above
+        {
+            get
+            {
+                return imyArray[i];
+            }
+            set
+            {
+                imyArray[i] = value;
+            }
+        }
+    }
+}
