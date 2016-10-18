43763c0f6606cfbd48cddd94927aa0f6acdaaf20
Dankoy <transingularity@gmail.com>
2016-10-18 21:02:45 +0300

    Added method marked with attribute as deprecated :)

--- Task2/Indexation/Program.cs
+++ Task2/Indexation/Program.cs
@@ -15,6 +15,12 @@ namespace Indexation
             obj[1] = 200;
             obj[2] = 300;
             Console.WriteLine("1 - {0}, 2 - {1}, 3 - {2}", obj[0], obj[1], obj[2]);
+            method();
+        }
+        [Obsolete("Warning message, showing by compiler.")]
+        public static void method()
+        {
+
         }
     }
 }
