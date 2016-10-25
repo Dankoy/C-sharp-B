4fdf56c0018768f5981b19d708d046c1f5e3ca86
Dankoy <transingularity@gmail.com>
2016-10-25 21:17:38 +0300

    Added method in abstract class and its overrided method in ClassAB with realisation

---
+++ Task3/AbstractClasses/AbstractClass1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    class AbstractClass1
+    {
+        public abstract int SFig(int a, int b);
+    }
+}
---
+++ Task3/AbstractClasses/ClassAB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    class ClassAB : AbstractClass1
+    {
+
+        public override int SFig(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
