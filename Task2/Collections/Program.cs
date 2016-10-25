913cdec868aedaaf57df7bfcbbc3a1608f6cb1a0
Dankoy <transingularity@gmail.com>
2016-10-25 20:57:18 +0300

    Added ArrayList + used some built-in methods for collections

--- Task2/Collections/Program.cs
+++ Task2/Collections/Program.cs
@@ -11,6 +11,19 @@ namespace Collections
     {
         static void Main(string[] args)
         {
+            ArrayList aList1 = new ArrayList();
+            aList1.Add("Collection");
+            aList1.Add("of type");
+            aList1.Add("ArrayList");
+
+            float f1 = 22.34F;
+            aList1.Add(f1);
+
+            Console.WriteLine("Collection: ");
+            Console.WriteLine("\tCount: {0}", aList1.Count);    //Amount of elements in collection
+            Console.WriteLine("\tCapacity: {0}", aList1.Capacity);  //Capacity of collection
+
+
         }
     }
 }
