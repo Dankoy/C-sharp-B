674b3574e6668271bc8c939e862604c53ddcf834
Dankoy <transingularity@gmail.com>
2016-10-25 21:39:01 +0300

    Added code using 'is' operator

--- Task3/AbstractClasses/Program.cs
+++ Task3/AbstractClasses/Program.cs
@@ -15,6 +15,18 @@ namespace AbstractClasses
             int b = 20;
 
             Console.WriteLine(varb1.SFig(a, b));
+
+            if (varb1 is Interface1)        // Using operator 'is'
+            {
+                Console.WriteLine("varb1 is a ref on class implemented Interface1");
+            }
+            Interface1 ivar = (Interface1)varb1;
+            if (ivar is Interface1)
+            {
+                Console.WriteLine("ivar is the ref to Interface1");
+            }
+            bool var1 = ivar.F1();
+            Console.WriteLine("Using method F1 from Interface1: {0}", var1);
         }
     }
 }
