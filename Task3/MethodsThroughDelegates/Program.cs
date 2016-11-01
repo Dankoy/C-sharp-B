c68562043e0b36cbad8f8d8f294946bcc46a5c8f
Dankoy <transingularity@gmail.com>
2016-11-01 21:37:41 +0300

    Added delegate instance and use it to call methods from CA class

--- Task3/MethodsThroughDelegates/Program.cs
+++ Task3/MethodsThroughDelegates/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
+delegate void MyDelegate();
 
 namespace MethodsThroughDelegates
 {
@@ -12,6 +12,19 @@ namespace MethodsThroughDelegates
     {
         static void Main(string[] args)
         {
+            CA var1 = new CA();
+
+            //Instance of MyDelegate
+            MyDelegate f_d = new MyDelegate(var1.f_Instance);
+
+            // Calling method f_Instance using delegate
+            f_d();
+
+            // New instance of delegate
+            f_d = new MyDelegate(CA.f_Static);
+
+            //Calling method f_Static using delegate
+            f_d();
         }
     }
 }
