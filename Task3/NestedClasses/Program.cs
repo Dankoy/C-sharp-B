e1487684df694bbbc92671ceaad74c291cd43f32
Dankoy <transingularity@gmail.com>
2016-11-01 20:48:33 +0300

    Added nested class and method in it

--- Task3/NestedClasses/Program.cs
+++ Task3/NestedClasses/Program.cs
@@ -10,7 +10,19 @@ namespace NestedClasses
     {
         static void Main(string[] args)
         {
+            Program.Class2 obj = new Program.Class2();
+            Console.WriteLine(obj.F1());
         }
-        
+        //Adding nested class
+        class Class2
+        {
+            // Can be seen only by nested class objects
+            private int i1 = 123;
+
+            public int F1()
+            {
+                return this.i1;
+            }
+        }
     }
 }
