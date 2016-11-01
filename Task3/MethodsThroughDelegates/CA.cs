eff01e4db2ea4f0ed59a4a25b823779e39075de5
Dankoy <transingularity@gmail.com>
2016-11-01 21:36:33 +0300

    Added class CA and two methods in it

---
+++ Task3/MethodsThroughDelegates/CA.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsThroughDelegates
+{
+    class CA
+    {
+        // Methods that will be used by delegate
+        public void f_Instance()
+        {
+            Console.WriteLine("Calling the method of class using delegate.");
+        }
+        public static void f_Static()
+        {
+            Console.WriteLine("Calling the static method using delegate.");
+        }
+    }
+}
