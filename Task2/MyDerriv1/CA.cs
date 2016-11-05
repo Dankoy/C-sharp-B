bd0b8dda5a27987dc409cc192e9f2f0466a4e11c
Evgeny Zelenin <transingularity@gmail.com>
2016-11-05 20:57:21 +0300

    Added new class CA and two methods within

---
+++ Task2/MyDerriv1/CA.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDerriv1
+{
+    public class CA
+    {
+        // Everybody can access this method
+        public int F1()
+        {
+            return 1;
+        }
+        // Only methods of tis class and its child classes methods can use this method
+        protected int F2()
+        {
+            return 2;
+        }
+    }
+}
