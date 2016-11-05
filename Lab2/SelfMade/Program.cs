856ecf6969c93688ed49d9c0bc9146a99fe688f3
Evgeny Zelenin <transingularity@gmail.com>
2016-11-05 20:27:46 +0300

    Added method that return particular element from matrix

--- Lab2/SelfMade/Program.cs
+++ Lab2/SelfMade/Program.cs
@@ -8,20 +8,27 @@ namespace SelfMade
 {
     class Program
     {
-        private static void InitializeMatrix(double[,] matrix, int XDim, int YDim)
+        // Initialize matrix
+        private static void InitializeMatrix(int[,] matrix, int XDim, int YDim)
         {
             Random rnd = new Random();
 
             for (int i = 0; i < XDim; ++i)
                 for (int j = 0; j < YDim; ++j)
                 {
-                    matrix[i, j] = E ? 0.00 : rnd.Next(1, 100);
+                    matrix[i, j] = rnd.Next(1, 100);
                 }
+        }
+        // Method returns matrix[i,j] element
+        private static int getElement1(int i, int j, int[,] matrix)
+        {
+            return matrix[i, j];
         }
         static void Main(string[] args)
         {
-            double[,] A1 = new double[10,10];
+            int[,] A1 = new int[10,10];
             InitializeMatrix(A1, 10, 10);
+            Console.WriteLine(getElement1(4, 6, A1));
         }
     }
 }
