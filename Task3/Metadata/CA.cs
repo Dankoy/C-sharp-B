530e6c177804947a93221880bd377b15a33d292c
Evgeny Zelenin <transingularity@gmail.com>
2016-11-20 17:54:44 +0300

    Added method that call for methadata of given Type

--- Task3/Metadata/CA.cs
+++ Task3/Metadata/CA.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Metadata
 {
+    struct struct1 { }
     class CA
     {
         public class CB_Inside_CA
--- Task3/Metadata/Program.cs
+++ Task3/Metadata/Program.cs
@@ -10,6 +10,25 @@ namespace Metadata
     {
         static void Main(string[] args)
         {
+            WhatIsType("System.Char");
+        }
+
+        // Method the call for metadata
+        public static void WhatIsType(string s)
+        {
+            try
+            {
+                Type tp = Type.GetType(s);
+                Console.WriteLine("Type name: {0}", tp.FullName);
+                Console.WriteLine("\tIs it class? {0}", tp.IsClass);
+                Console.WriteLine("\tIs it abstract class? {0}", tp.IsAbstract);
+                Console.WriteLine("\tIs it inner and public? {0}", tp.IsNestedPublic);
+                Console.WriteLine("\tIs it final? {0}", tp.IsSealed);
+            }
+            catch (System.NullReferenceException)
+            {
+                Console.WriteLine("Type error");
+            }
         }
     }
 }
