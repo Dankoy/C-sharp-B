bc5571ddcdc0decb03888909c704e490a8b803f6
Evgeny Zelenin <transingularity@gmail.com>
2016-11-20 17:14:23 +0300

    Added new method

--- Lab2/SelfMade/Program.cs
+++ Lab2/SelfMade/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace SelfMade
 {
@@ -16,7 +17,7 @@ namespace SelfMade
             for (int i = 0; i < XDim; ++i)
                 for (int j = 0; j < YDim; ++j)
                 {
-                    matrix[i, j] = rnd.Next(1, 100);
+                    matrix[i, j] = rnd.Next(1, 10);
                 }
         }
         // Method returns matrix[i,j] element
@@ -28,6 +29,11 @@ namespace SelfMade
         private static void getElement2(ref int elemen, int i, int j, int[,] matrix) {
             elemen = matrix[i, j];
         }
+        // Method returns i,j using references and LINQ
+        private static void getElement3(ref int i, ref int j, int[][] matrix, int find_number)
+        {
+            
+        }
         static void Main(string[] args)
         {
             int[,] A1 = new int[10,10];
@@ -38,6 +44,14 @@ namespace SelfMade
             getElement2(ref elem, 4, 6, A1);
             Console.WriteLine(elem);
 
+            int[,] A2 = new int[20, 20];
+            InitializeMatrix(A2, 20, 20);
+
+            // Adding reference elements
+            int refI = 0;
+            int refJ = 0;
+            // The number we search for:
+            int find_num = 4;
 
         }
     }
