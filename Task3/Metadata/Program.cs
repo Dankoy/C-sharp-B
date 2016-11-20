bb0443d8178e33a104a4c82aacc81f7dcc3c6229
Evgeny Zelenin <transingularity@gmail.com>
2016-11-20 18:17:19 +0300

    Added code that print all methods and its parameters of class

--- Task3/Metadata/Program.cs
+++ Task3/Metadata/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +16,29 @@ namespace Metadata
             WhatIsType("Metadata.Program");
             WhatIsType("Metadata.CA");
             WhatIsType("Metadata.CA+CB_Inside_CA");
+
+            // Recieving information about methods using reflection
+            CA ca = new CA();
+            Type tCA = Type.GetType("Metadata.CA");
+            Console.WriteLine("Type of CA class: " + tCA);
+
+            Console.WriteLine();
+
+            MethodInfo[] miCA = tCA.GetMethods();
+            Console.WriteLine("Class CA. Amount of methods: {0}", miCA.Length);
+            Console.WriteLine();
+
+            foreach (MethodInfo met in miCA)
+            {
+                Console.WriteLine(met.Name);
+                Console.WriteLine("Return type = {0}", met.ReturnType.FullName);
+                foreach (ParameterInfo par in met.GetParameters())
+                {
+                    Console.WriteLine("Parameter name - {0}, parameter type {1}", par.Name, par.ParameterType);
+                }
+                Console.WriteLine();
+            }
+
         }
 
         // Method the call for metadata
