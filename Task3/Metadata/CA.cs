c8a993f30129f15057534ce6022fc95fd265753f
Evgeny Zelenin <transingularity@gmail.com>
2016-11-20 17:45:07 +0300

    Added three methods in CA and inner class CB in CA

--- Task3/Metadata/CA.cs
+++ Task3/Metadata/CA.cs
@@ -8,5 +8,24 @@ namespace Metadata
 {
     class CA
     {
+        public class CB_Inside_CA
+        {
+
+        }
+
+        public void Method1()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Method2(int i1, ref int i2)
+        {
+            i2 = i1 + i2;
+        }
+
+        public int Method3(string s1)
+        {
+            return s1.Length;
+        }
     }
 }
