2fdefa0aa7eb5bfa3d2ae65a49db6b408a135149
Evgeny Zelenin <transingularity@gmail.com>
2017-06-27 19:01:49 +0300

    Forgot to commit these files lol

---
+++ Task3/SelfMade/CA.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfMade
+{
+    struct struct1 { }
+    class CA
+    {
+        public class CB_Inside_CA
+        {
+
+        }
+
+        public void Method1()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Method2(int i1, ref int i2)
+        {
+            i2 = i1 + i2;
+        }
+
+        public int Method3(string s1)
+        {
+            return s1.Length;
+        }
+    }
+}
