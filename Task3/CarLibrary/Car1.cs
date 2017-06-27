52e77cb1a8367e8466b7dd7a36c545a1d39f9fc7
Evgeny Zelenin <transingularity@gmail.com>
2017-06-27 22:38:51 +0300

    Added DLL library for late binding

---
+++ Task3/CarLibrary/Car1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLibrary
+{
+    public class Car1 : Car {
+		public Car1(){}
+		public Car1(string name, short max, short curr) : base(name,max,curr){}
+		public override void Turbo() {
+			MessageBox.Show("Ramming speed!", "Faster is better");
+		}
+	}
+	public class Car2 : Car {
+		public Car2(){}
+		public Car2(string name, short max, short curr) : base(name,max,curr){}
+		public override void Turbo() {
+			egnState = EngineState.engineDead;
+			MessageBox.Show("AAAAA", "11111111111");
+		}
+		public void BeQuiet(string kidName, int numb) {
+			for(int i = 0; i<numb; i++) {
+				MessageBox.Show("Be quiet " + kidName + "!!");
+			}
+		}
+}
--- Task3/LateBinding/Program.cs
+++ Task3/LateBinding/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,44 @@ namespace LateBinding
     {
         static void Main(string[] args)
         {
+            //Tryine to load local copy of dll
+            Console.WriteLine("--- Late Binding ---\n");
+            Assembly a = null;
+            try
+            {
+                //CarLibrary in bin\debug
+                a = Assembly.Load("CarLibrary");
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+                return;
+            }
+            
+            // Asking a type of Car2 from Assembly
+            Type car = a.GetType("CarLibrary.Car2");
+
+            // Creating an object Car2 on the go (using Activator class)
+            object obj = Activator.CreateInstance(car);
+
+            //Asking info about next method 
+            MethodInfo mi = car.GetMethod("Turbo");
+
+            //Calling this method
+            mi.Invoke(obj, null); // without parameter
+
+            //Create array of params
+            object[] paramArray = new object[2];
+            paramArray[0] = "Fred";
+            paramArray[1] = 4;
+            mi = car.GetMethod("BeQuiet");
+
+            //Calling this method using late binding mechanism
+            mi.Invoke(obj, paramArray);
+
+            Console.WriteLine("Main ended");
+            Console.ReadLine();
         }
     }
 }
