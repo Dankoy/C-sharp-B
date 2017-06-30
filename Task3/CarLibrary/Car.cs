42f86fa2a352d10ab5392318934d5e88cb4497bf
Evgeny Zelenin <transingularity@gmail.com>
2017-06-30 17:37:12 +0300

    Added class Car to make CarLibrary.dll work.

---
+++ Task3/CarLibrary/Car.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarLibrary
+{
+
+    public enum EngineState {  engineAlive, engineDead }
+    public enum MusicMedia { musicCd, musicTape, musicRadio, musicMp3 }
+
+    public abstract class Car
+    {
+        protected string petName;
+        protected short currSpeed;
+        protected short maxSpeed;
+        protected EngineState egnState = EngineState.engineAlive;
+
+        public Car()
+        {
+            MessageBox.Show("Car 2.0.0.0");
+        }
+            public Car(string name, short max, short curr) {
+                MessageBox.Show("Car 2.0.0.0");
+                petName = name;
+                maxSpeed = max;
+                currSpeed = curr;
+            }
+            public string PetName
+            {
+                get { return petName; }
+                set { petName = value; }
+            }
+            public short CurrSpeed
+            {
+                get { return currSpeed; }
+                set { currSpeed = value; }
+            }
+            public short MaxSpeed
+            {
+                get { return maxSpeed; }
+            }
+
+            public EngineState EngineState
+            {
+                get { return egnState; }
+            }
+            public abstract void Turbo();
+            public void TurnOnRadio(bool musicOn, MusicMedia mm)
+            {
+                if (musicOn)
+                {
+                    MessageBox.Show(string.Format("Jamming {0}", mm.ToString()));
+                }
+                else
+                {
+                    MessageBox.Show("Quiet time...");
+                }
+            }
+    }
+}
--- Task3/CarLibrary/Car1.cs
+++ Task3/CarLibrary/Car1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace CarLibrary
 {
@@ -13,16 +14,21 @@ namespace CarLibrary
 			MessageBox.Show("Ramming speed!", "Faster is better");
 		}
 	}
-	public class Car2 : Car {
-		public Car2(){}
-		public Car2(string name, short max, short curr) : base(name,max,curr){}
-		public override void Turbo() {
-			egnState = EngineState.engineDead;
-			MessageBox.Show("AAAAA", "11111111111");
-		}
-		public void BeQuiet(string kidName, int numb) {
-			for(int i = 0; i<numb; i++) {
-				MessageBox.Show("Be quiet " + kidName + "!!");
-			}
-		}
+    public class Car2 : Car
+    {
+        public Car2() { }
+        public Car2(string name, short max, short curr) : base(name, max, curr) { }
+        public override void Turbo()
+        {
+            egnState = EngineState.engineDead;
+            MessageBox.Show("AAAAA", "11111111111");
+        }
+        public void BeQuiet(string kidName, int numb)
+        {
+            for (int i = 0; i < numb; i++)
+            {
+                MessageBox.Show("Be quiet " + kidName + "!!");
+            }
+        }
+    }
 }
