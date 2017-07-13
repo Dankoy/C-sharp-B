c88960c0431ef73d5be9bf3b39c2f2f2c80f1526
Evgeny Zelenin <transingularity@gmail.com>
2017-07-13 13:06:44 +0300

    Added code for new buttons

--- Task4_WF_VS2015/Task4_WF_VS2015/Form1.Designer.cs
+++ Task4_WF_VS2015/Task4_WF_VS2015/Form1.Designer.cs
@@ -105,7 +105,7 @@
             this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.panel2.Location = new System.Drawing.Point(435, 14);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(403, 519);
+            this.panel2.Size = new System.Drawing.Size(403, 424);
             this.panel2.TabIndex = 1;
             // 
             // panel3
@@ -130,7 +130,7 @@
             this.panel3.Controls.Add(this.button4);
             this.panel3.Location = new System.Drawing.Point(12, 96);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(374, 437);
+            this.panel3.Size = new System.Drawing.Size(374, 342);
             this.panel3.TabIndex = 2;
             // 
             // button4
@@ -151,6 +151,7 @@
             this.button5.TabIndex = 1;
             this.button5.Text = "5. Draw rectangle";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -160,6 +161,7 @@
             this.button6.TabIndex = 2;
             this.button6.Text = "6. Draw filled polygon";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
@@ -169,6 +171,7 @@
             this.button7.TabIndex = 3;
             this.button7.Text = "7. Write text";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
@@ -178,6 +181,7 @@
             this.button8.TabIndex = 4;
             this.button8.Text = "8. Write text in point";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
@@ -187,6 +191,7 @@
             this.button9.TabIndex = 5;
             this.button9.Text = "9. Draw square in point with known side";
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button10
             // 
@@ -194,51 +199,54 @@
             this.button10.Name = "button10";
             this.button10.Size = new System.Drawing.Size(195, 34);
             this.button10.TabIndex = 6;
-            this.button10.Text = "11. Draw ellipse in point with known sides";
+            this.button10.Text = "10. Draw ellipse in point with known sides";
             this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // button11
             // 
-            this.button11.Location = new System.Drawing.Point(13, 322);
+            this.button11.Location = new System.Drawing.Point(6, 244);
             this.button11.Name = "button11";
             this.button11.Size = new System.Drawing.Size(134, 38);
             this.button11.TabIndex = 7;
             this.button11.Text = "11. Get measurements of panel 3";
             this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
             // 
             // button12
             // 
-            this.button12.Location = new System.Drawing.Point(194, 322);
+            this.button12.Location = new System.Drawing.Point(6, 288);
             this.button12.Name = "button12";
             this.button12.Size = new System.Drawing.Size(134, 38);
             this.button12.TabIndex = 8;
             this.button12.Text = "12. Get measurements of Form";
             this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(13, 254);
+            this.textBox1.Location = new System.Drawing.Point(204, 164);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(54, 20);
             this.textBox1.TabIndex = 9;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(95, 254);
+            this.textBox2.Location = new System.Drawing.Point(204, 203);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(54, 20);
             this.textBox2.TabIndex = 10;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(221, 254);
+            this.textBox3.Location = new System.Drawing.Point(290, 164);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(54, 20);
             this.textBox3.TabIndex = 11;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(301, 254);
+            this.textBox4.Location = new System.Drawing.Point(290, 203);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(54, 20);
             this.textBox4.TabIndex = 12;
@@ -246,7 +254,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(10, 238);
+            this.label1.Location = new System.Drawing.Point(204, 148);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(14, 13);
             this.label1.TabIndex = 13;
@@ -255,7 +263,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(92, 238);
+            this.label2.Location = new System.Drawing.Point(204, 187);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(14, 13);
             this.label2.TabIndex = 14;
@@ -264,7 +272,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(252, 238);
+            this.label3.Location = new System.Drawing.Point(279, 148);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(76, 13);
             this.label3.TabIndex = 15;
@@ -272,23 +280,23 @@
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(13, 376);
+            this.textBox5.Location = new System.Drawing.Point(177, 254);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(81, 20);
             this.textBox5.TabIndex = 16;
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(110, 376);
+            this.textBox6.Location = new System.Drawing.Point(177, 288);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(88, 20);
+            this.textBox6.Size = new System.Drawing.Size(81, 20);
             this.textBox6.TabIndex = 17;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(863, 538);
+            this.ClientSize = new System.Drawing.Size(855, 450);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
--- Task4_WF_VS2015/Task4_WF_VS2015/Form1.cs
+++ Task4_WF_VS2015/Task4_WF_VS2015/Form1.cs
@@ -85,5 +85,74 @@ namespace Task4_WF_VS2015
             g.DrawEllipse(pen, 10, 10, 60, 60);     // g - graphical context
             pen.Dispose();  // Freeing pen
         }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Pen pen = new Pen(Color.Red);
+            pen.LineJoin = System.Drawing.Drawing2D.LineJoin.Bevel;
+            pen.Width = 8F;
+            g.DrawRectangle(pen, 10, 80, 70, 50);
+            pen.Dispose();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Brush myBrush = Brushes.GreenYellow;
+            g.FillRectangle(myBrush, 10, 150, 70, 50);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Font myFont = new Font("Helvetica", 22);
+            Brush myBrush = Brushes.DarkOrchid;
+            g.DrawString("Hello, World", myFont, myBrush, 10, 200);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Font myFont = new Font("Helvetica", 22);
+            Brush myBrush = Brushes.DarkOrchid;
+            g.DrawString("Arbitrary string", myFont, myBrush, Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Pen pen = new Pen(Color.Blue);
+            pen.LineJoin = System.Drawing.Drawing2D.LineJoin.Bevel; // Скошенные углы
+            pen.Width = 2.0F;
+            g.DrawRectangle(pen,
+                Convert.ToInt32(textBox1.Text),
+                Convert.ToInt32(textBox2.Text),
+                Convert.ToInt32(textBox3.Text),
+                Convert.ToInt32(textBox4.Text)); // Параметры: (перо, x, y, ширина, высота)
+            pen.Dispose();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Pen pen = new Pen(Color.Tomato);
+            g.DrawEllipse(pen,
+                Convert.ToSingle(textBox1.Text),
+                Convert.ToSingle(textBox2.Text),
+                Convert.ToSingle(textBox3.Text),
+                Convert.ToSingle(textBox4.Text));
+            pen.Dispose(); // Освобождение пера
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox5.Text = Convert.ToString(this.panel3.Size.Width);
+            textBox6.Text = Convert.ToString(this.panel3.Size.Height);
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            textBox5.Text = Convert.ToString(this.Size.Width);
+            textBox6.Text = Convert.ToString(this.Size.Height);
+        }
+
+        //
+        // Finishing of second task
+        //
     }
 }
