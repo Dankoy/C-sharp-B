938a3bcee16a7f10ca4763fa445eb1490c2911da
Evgeny Zelenin <transingularity@gmail.com>
2017-07-13 13:52:37 +0300

    Added buttons and things

--- Task4_WF_VS2015/3-MessageBox/Form1.cs
+++ Task4_WF_VS2015/3-MessageBox/Form1.cs
@@ -16,5 +16,16 @@ namespace _3_MessageBox
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.String s1 = Convert.ToString(System.DateTime.Now);
+            textBox1.Text = s1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Message", "Header", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
     }
 }
