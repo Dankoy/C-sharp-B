7e38e425cc40f742320193014030585b45c1e0a7
Evgeny Zelenin <transingularity@gmail.com>
2017-07-20 15:25:43 +0300

    Added lists showing when you push button

--- Task4_WF_VS2015/7-Lists/Form1.Designer.cs
+++ Task4_WF_VS2015/7-Lists/Form1.Designer.cs
@@ -28,12 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBoxAr = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxAr
+            // 
+            this.listBoxAr.FormattingEnabled = true;
+            this.listBoxAr.Location = new System.Drawing.Point(43, 39);
+            this.listBoxAr.Name = "listBoxAr";
+            this.listBoxAr.Size = new System.Drawing.Size(120, 95);
+            this.listBoxAr.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(43, 157);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(43, 196);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(120, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(43, 239);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Add elements to list";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 370);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBoxAr);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBoxAr;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Task4_WF_VS2015/7-Lists/Form1.cs
+++ Task4_WF_VS2015/7-Lists/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,32 @@ namespace _7_Lists
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // List<MyType> arrlist = new List<MyType>(); - parametrical collection
+
+            ArrayList arrlist = new ArrayList();
+            arrlist.Add(new MyType("Name 1", "1333344"));
+            arrlist.Add(new MyType("Name 2", "4547687"));
+            arrlist.Add(new MyType("Name 3", "1335435"));
+            arrlist.Add(new MyType("Name 4", "5654243"));
+
+            // listBoxAr.Enabled = false; - if you want to make list unable
+            listBoxAr.DataSource = arrlist;     // Binding the compontnt with sourse
+            listBoxAr.DisplayMember = "Name";   // Show the pointing value
+            listBoxAr.ValueMember = "Phone";    // value that will be given to you as value of list
+        }
+
+        private void listBoxAr_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBoxAr.SelectedIndex != -1)  // Does this element exist?
+            {
+                textBox1.Text = listBoxAr.SelectedValue.ToString();
+                string s = ((MyType)listBoxAr.SelectedItem).Name;
+                textBox2.Text = s;
+            }
         }
     }
 }
