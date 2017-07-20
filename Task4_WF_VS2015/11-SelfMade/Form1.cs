f247ed48e19848d949f65e4e929a5e3b1512f87e
Evgeny Zelenin <transingularity@gmail.com>
2017-07-20 16:50:48 +0300

    Added 3-7 buttons and code for them

--- Task4_WF_VS2015/11-SelfMade/Form1.cs
+++ Task4_WF_VS2015/11-SelfMade/Form1.cs
@@ -27,5 +27,38 @@ namespace _11_SelfMade
             string s = (this.checkBox1.Checked ? this.checkBox1.Name : "") + " " + (this.checkBox2.Checked ? this.checkBox2.Name : "");
             this.textBox2.Text = s;
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.richTextBox1.Text += this.textBox3.Text + "\n";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+                this.richTextBox1.SaveFile(dialog.FileName);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+                this.richTextBox1.LoadFile(dialog.FileName);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            dialog.ShowDialog();
+            this.richTextBox1.SelectionColor = dialog.Color;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            dialog.ShowDialog();
+            this.richTextBox1.SelectionBackColor = dialog.Color;
+        }
     }
 }
