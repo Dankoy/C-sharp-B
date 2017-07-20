ba1a162c24d656bc22279c0d268925b02119d7d6
Evgeny Zelenin <transingularity@gmail.com>
2017-07-20 15:13:05 +0300

    Added class MyType

---
+++ Task4_WF_VS2015/7-Lists/MyType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Lists
+{
+    class MyType
+    {
+        private string myName;
+        private string myPhone;
+        public MyType(string strName, string strPhone)
+        {
+            myName = strName;
+            myPhone = strPhone;
+        }
+        public string Name { get { return myName; } }
+        public string Phone { get { return myPhone; } }
+    }
+}
