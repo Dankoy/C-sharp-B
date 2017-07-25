da3b6477b90e8ce31217fe24258f111d7e6de49b
Evgeny Zelenin <transingularity@gmail.com>
2017-07-25 17:58:19 +0300

    Added project about Application properties, can be seen through all the application

---
+++ Task4_WPF/3-ApplicationProperties/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _3_ApplicationProperties
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Application.Current.Properties["p2"] = "123";       // This properties can be seen on the second window
+            Application.Current.Properties["p1"] = "abc";       // This too
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 w1 = new Window1();
+            w1.Show();
+        }
+    }
+}
---
+++ Task4_WPF/3-ApplicationProperties/Window1.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _3_ApplicationProperties
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            t2.Text = Application.Current.Properties["p1"].ToString() + "\r\n";
+            t2.AppendText(Application.Current.Properties["p2"].ToString());
+        }
+    }
+}
