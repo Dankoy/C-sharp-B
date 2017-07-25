e30e503a6f0c15d5d9960c12e295b43eaaaa164e
Evgeny Zelenin <transingularity@gmail.com>
2017-07-25 13:27:49 +0300

    Added file and button that copy that file from one directory to another

--- Task4_WPF/2-WritingFiles/MainWindow.xaml.cs
+++ Task4_WPF/2-WritingFiles/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace _2_WritingFiles
 {
@@ -23,6 +24,25 @@ namespace _2_WritingFiles
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            string StartDirectory = @"C:\Users\Evgeny\VS Projects\C-sharp-B\Task4_WPF\2-WritingFiles\Copy"; //Where we copy things from
+            string EndDirectory = @"C:\Users\Evgeny\VS Projects\C-sharp-B\Task4_WPF\2-WritingFiles\Paste";   //Where we copy things to
+            this.t1.Clear();    // Clearing textbox1
+
+            foreach (string filename in Directory.EnumerateFiles(StartDirectory))
+            {
+                this.t1.AppendText(filename + "\r\n"); //Add name of copying file in textbox
+                using (FileStream SourceStream = File.Open(filename, FileMode.Open))
+                {
+                    using (FileStream DestinationStream = File.Create(EndDirectory + filename.Substring(filename.LastIndexOf('\\'))))
+                    {
+                        await SourceStream.CopyToAsync(DestinationStream);  // Copying file
+                    }
+                }
+            }
         }
     }
 }
