faa6cf504f14517c6c8a625a14e8f0969ac512fd
Evgeny Zelenin <transingularity@gmail.com>
2017-10-06 22:49:42 +0300

    Added oleDbDataAdapter and SQL code

--- Task5_ADO_NEW/OleDbDataAdapter_Usage/Form1.Designer.cs
+++ Task5_ADO_NEW/OleDbDataAdapter_Usage/Form1.Designer.cs
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.oleDbSelectCommand1 = new System.Data.OleDb.OleDbCommand();
+            this.oleDbInsertCommand1 = new System.Data.OleDb.OleDbCommand();
+            this.oleDbDataAdapter1 = new System.Data.OleDb.OleDbDataAdapter();
+            this.oleDbConnection1 = new System.Data.OleDb.OleDbConnection();
+            this.SuspendLayout();
+            // 
+            // oleDbSelectCommand1
+            // 
+            this.oleDbSelectCommand1.CommandText = "SELECT        f1, f2, f3\r\nFROM            Table_1";
+            this.oleDbSelectCommand1.Connection = this.oleDbConnection1;
+            // 
+            // oleDbInsertCommand1
+            // 
+            this.oleDbInsertCommand1.CommandText = "INSERT INTO [Table_1] ([f1], [f2], [f3]) VALUES (?, ?, ?)";
+            this.oleDbInsertCommand1.Connection = this.oleDbConnection1;
+            this.oleDbInsertCommand1.Parameters.AddRange(new System.Data.OleDb.OleDbParameter[] {
+            new System.Data.OleDb.OleDbParameter("f1", System.Data.OleDb.OleDbType.WChar, 0, "f1"),
+            new System.Data.OleDb.OleDbParameter("f2", System.Data.OleDb.OleDbType.WChar, 0, "f2"),
+            new System.Data.OleDb.OleDbParameter("f3", System.Data.OleDb.OleDbType.WChar, 0, "f3")});
+            // 
+            // oleDbDataAdapter1
+            // 
+            this.oleDbDataAdapter1.InsertCommand = this.oleDbInsertCommand1;
+            this.oleDbDataAdapter1.SelectCommand = this.oleDbSelectCommand1;
+            this.oleDbDataAdapter1.TableMappings.AddRange(new System.Data.Common.DataTableMapping[] {
+            new System.Data.Common.DataTableMapping("Table", "Table_1", new System.Data.Common.DataColumnMapping[] {
+                        new System.Data.Common.DataColumnMapping("f1", "f1"),
+                        new System.Data.Common.DataColumnMapping("f2", "f2"),
+                        new System.Data.Common.DataColumnMapping("f3", "f3")})});
+            // 
+            // oleDbConnection1
+            // 
+            this.oleDbConnection1.ConnectionString = "Provider=SQLNCLI11;Data Source=EVGENY-PC;Integrated Security=SSPI;Initial Catalog" +
+    "=Mydb";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Data.OleDb.OleDbCommand oleDbSelectCommand1;
+        private System.Data.OleDb.OleDbCommand oleDbInsertCommand1;
+        private System.Data.OleDb.OleDbDataAdapter oleDbDataAdapter1;
+        private System.Data.OleDb.OleDbConnection oleDbConnection1;
     }
 }
 
--- Task5_ADO_NEW/OleDbDataAdapter_Usage/Form1.cs
+++ Task5_ADO_NEW/OleDbDataAdapter_Usage/Form1.cs
@@ -16,5 +16,10 @@ namespace OleDbDataAdapter_Usage
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
