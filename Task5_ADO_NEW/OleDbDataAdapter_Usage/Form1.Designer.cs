6a7c122f45ad24ac4c0178bcbc4a10991e4e0e0c
Evgeny Zelenin <transingularity@gmail.com>
2017-10-06 23:59:34 +0300

    Using the parametrical querry

--- Task5_ADO_NEW/OleDbDataAdapter_Usage/Form1.Designer.cs
+++ Task5_ADO_NEW/OleDbDataAdapter_Usage/Form1.Designer.cs
@@ -36,14 +36,18 @@
             this.dataSet11 = new OleDbDataAdapter_Usage.DataSet1();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.dataSet11BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataSet11)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataSet11BindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // oleDbSelectCommand1
             // 
-            this.oleDbSelectCommand1.CommandText = "SELECT        f1, f2, f3\r\nFROM            Table_1";
+            this.oleDbSelectCommand1.CommandText = "SELECT        f1, f2, f3\r\nFROM            Table_1\r\nWHERE        (f1 >= ?)";
             this.oleDbSelectCommand1.Connection = this.oleDbConnection1;
+            this.oleDbSelectCommand1.Parameters.AddRange(new System.Data.OleDb.OleDbParameter[] {
+            new System.Data.OleDb.OleDbParameter("Parameter1", System.Data.OleDb.OleDbType.Numeric)});
             // 
             // oleDbInsertCommand1
             // 
@@ -79,7 +83,7 @@
             this.listBox1.DataSource = this.dataSet11BindingSource;
             this.listBox1.DisplayMember = "f2";
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Location = new System.Drawing.Point(24, 83);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(244, 95);
             this.listBox1.TabIndex = 0;
@@ -90,11 +94,30 @@
             this.dataSet11BindingSource.DataMember = "Table_1";
             this.dataSet11BindingSource.DataSource = this.dataSet11;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(157, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Show DataSet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(566, 376);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.listBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -102,6 +125,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataSet11)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataSet11BindingSource)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -114,6 +138,8 @@
         private DataSet1 dataSet11;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.BindingSource dataSet11BindingSource;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Task5_ADO_NEW/OleDbDataAdapter_Usage/Form1.cs
+++ Task5_ADO_NEW/OleDbDataAdapter_Usage/Form1.cs
@@ -19,6 +19,14 @@ namespace OleDbDataAdapter_Usage
 
         private void Form1_Load(object sender, EventArgs e)
         {
+  //          oleDbDataAdapter1.Fill(this.dataSet11);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int par = Int32.Parse(this.textBox1.Text);
+            oleDbDataAdapter1.SelectCommand.Parameters[0].Value = par;
+            this.dataSet11.Clear();
             oleDbDataAdapter1.Fill(this.dataSet11);
         }
     }
